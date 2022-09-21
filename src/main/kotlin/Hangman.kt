e099df9506d966e65538e47004a197a125a74b8a
FI-User <fi-user@Fiusers-MacBook-Pro.local>
2022-09-21 17:31:05 +0530

    Changes updated

--- src/main/kotlin/Hangman.kt
+++ src/main/kotlin/Hangman.kt
@@ -1,62 +1,60 @@
 import kotlin.random.Random
 
-val words=listOf("India","England","Porsche","Nissan","Cinema","AR Rahman","Aniruth","Yuvan","Udmey")
-var track=""
-val guesses=arrayListOf<Char>()
-var remainingGuesses=6
-var mistakes=0
+val words = listOf("India", "England", "Porsche", "Nissan", "Cinema", "AR Rahman", "Aniruth", "Yuvan", "Udmey")
+var track = ""
+val guesses = arrayListOf<Char>()
+var remainingGuesses = 6
+var mistakes = 0
 
 
 fun main() {
     setupGame()
 }
-
-fun setupGame(){
-    val wordIndex=Random.nextInt(words.size)
-    track=words[wordIndex].toUpperCase()
+fun setupGame() {
+    // Getting a random set of word from th array list declared at the beginning
+    val wordIndex = Random.nextInt(words.size)
+    track = words[wordIndex].toUpperCase()
     println(track)
 
-    for(i in track.indices)
+    // Setting up of dash for guessing the word
+    for (i in track.indices)
         guesses.add('_')
 
-
-    var gameOver=false
+    // Initialising a variable
+    var gameOver = false
 
     do {
         printGameStatus()
         println("Please enter a letter:")
-        val input=readLine()?:""
+        val input = readLine() ?: ""
 
-        if(input.isEmpty()) {
+        if (input.isEmpty()) {
             println("Not a valid input. Please try again!")
-        }
-        else {
-            val letter=input[0].toUpperCase()
-            if(track.contains(letter)) {
-                for( i in track.indices){
-                    if(track[i]==letter)
-                        guesses[i]=letter
+        } else {
+            val letter = input[0].toUpperCase()
+            if (track.contains(letter)) {
+                for (i in track.indices) {
+                    if (track[i] == letter)
+                        guesses[i] = letter
                 }
-                if(!guesses.contains('_'))
-                    gameOver=true
-            }
-            else {
+                if (!guesses.contains('_'))
+                    gameOver = true
+            } else {
                 println("Sorry that not the part of the word :( ")
                 remainingGuesses--
                 mistakes++
-                if(mistakes==6) {
-                    gameOver=true
+                if (mistakes == 6) {
+                    gameOver = true
                 }
             }
         }
 
-    }while(!gameOver)
+    } while (!gameOver)
 
-    if(mistakes==6){
+    if (mistakes == 6) {
         printGameStatus()
         println("Sorry You Lost the Game. The word was \n $track")
-    }
-    else {
+    } else {
         println("Congratulations, you win !!!")
         println("The word was \n $track")
     }
@@ -64,7 +62,7 @@ fun setupGame(){
 }
 
 fun printGameStatus() {
-    when(mistakes) {
+    when (mistakes) {
         0 -> print0Mistakes()
         1 -> print1Mistakes()
         2 -> print2Mistakes()
@@ -74,13 +72,13 @@ fun printGameStatus() {
         6 -> print6Mistakes()
     }
     print("Word: ")
-    for(i in guesses)
+    for (i in guesses)
         print("$i ")
     println("\nYou have $remainingGuesses guess(es) left")
 
 }
 
-fun print0Mistakes(){
+fun print0Mistakes() {
     println("  |------|-")
     println("  |      | ")
     println("  |        ")
@@ -123,6 +121,7 @@ fun print3Mistakes() {
     println(" /|\\      ")
     println("/ | \\     ")
 }
+
 fun print4Mistakes() {
     println("  |------|-")
     println("  |      | ")
@@ -133,6 +132,7 @@ fun print4Mistakes() {
     println(" /|\\      ")
     println("/ | \\     ")
 }
+
 fun print5Mistakes() {
     println("  |------|-")
     println("  |      | ")
@@ -143,6 +143,7 @@ fun print5Mistakes() {
     println(" /|\\      ")
     println("/ | \\     ")
 }
+
 fun print6Mistakes() {
     println("  |------|-")
     println("  |      | ")
